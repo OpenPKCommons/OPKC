5c29decbeb43fb1d53ff8950aafe3bfbff13fa80
Stephen Kissler <skissler@hsph.harvard.edu>
2025-05-24 05:50:48 -0600

    adding some code to ingest studies

---
+++ code/create_schema.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+import ingest_ke
+import ingest_kissler
+import ingest_russell
+import ingest_katz
+import ingest_wagstaffe
+
+# Replace 'your_file.xlsx' with the path to your Excel file
+# file_path = 'your_file.xlsx'
+
+# Load the Excel file (reads the first sheet by default)
+# df = pd.read_excel(file_path)
+
+# If you want to specify a particular sheet
+# df = pd.read_excel(file_path, sheet_name='Sheet1')
+
+# Display the first few rows
+# print(df.head())
