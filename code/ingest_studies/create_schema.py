8e685e43dfd9f7b8c6208e7e1d75720340b6214a
Stephen Kissler <skissler@hsph.harvard.edu>
2025-05-28 09:56:28 -0600

    Updated study names and re-routed the resulting dataset to the output/ directory

--- code/ingest_studies/create_schema.py
+++ code/ingest_studies/create_schema.py
@@ -1,16 +1,18 @@
 # from studies import study1, study2, study3
-from studies import study1
+from studies import ke2022, kissler2023, russell2024, wagstaffe2024, wongnak2024
 from schema import enforce_schema, coerce_types
 import pandas as pd
 
 def main():
-    df1 = study1.load_and_format()
-    # df2 = study2.load_and_format()
-    # df3 = study3.load_and_format()
+    df1 = ke2022.load_and_format()
+    # df2 = kissler2023.load_and_format()
+    # df3 = russell2024.load_and_format()
+    # df4 = wagstaffe2024.load_and_format()
+    # df5 = wongnak2024.load_and_format()
 
     # combined_df = pd.concat([df1, df2, df3])
     combined_df = df1
-    combined_df.to_csv("combined_cleaned_data.csv", index=False)
+    combined_df.to_csv("output/combined_cleaned_data.csv", index=False)
 
 if __name__ == "__main__":
     main()
--- code/ingest_studies/studies/study2.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from schema import standardize_headers
-
-def load_and_format():
-    # Replace with the actual path to your dataset
-    df = pd.read_excel("data/study2_data.xlsx")
-    df = standardize_headers(df)
-    return df
