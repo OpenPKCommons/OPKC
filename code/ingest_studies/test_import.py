8668d091d2e3e76d0394cc1c9cb19a7b5964fdf0
Stephen Kissler <skissler@hsph.harvard.edu>
2025-10-03 23:12:58 -0600

    started editing the wongnak2024.py digestion script. So far it just imports a selection of the columns from the raw csv. test_import.py is updated to accommodate wongnak2024 now, too.

--- code/ingest_studies/test_import.py
+++ code/ingest_studies/test_import.py
@@ -3,9 +3,9 @@ from schema import enforce_schema, coerce_types
 import pandas as pd
 
 def main():
-    df_russell2024 = russell2024.load_and_format()
+    df_wongnak2024 = wongnak2024.load_and_format()
     
-    df_russell2024.to_csv("output/test_import.csv", index=False)
+    df_wongnak2024.to_csv("output/test_import.csv", index=False)
 
 if __name__ == "__main__":
     main()
