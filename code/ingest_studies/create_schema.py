eb5007d18a6f69354195edd834443bab85da303a
Stephen Kissler <skissler@hsph.harvard.edu>
2025-10-05 21:32:08 -0600

    Got wongnak integrated into the full dataset.

--- code/ingest_studies/create_schema.py
+++ code/ingest_studies/create_schema.py
@@ -7,11 +7,11 @@ def main():
     df_kissler2023 = kissler2023.load_and_format()
     df_russell2024 = russell2024.load_and_format()
     # df_wagstaffe2024 = wagstaffe2024.load_and_format()
-    # df_wongnak2024 = wongnak2024.load_and_format()
+    df_wongnak2024 = wongnak2024.load_and_format()
 
     # combined_df = pd.concat([df_ke2022, df_kissler2023, df_russell2024, df_wagstaffe2024, df_wongnak2024])
     # combined_df = df_ke2022
-    combined_df = pd.concat([df_ke2022, df_kissler2023, df_russell2024])
+    combined_df = pd.concat([df_ke2022, df_kissler2023, df_russell2024, df_wongnak2024])
     combined_df.to_csv("output/combined_cleaned_data.csv", index=False)
 
 if __name__ == "__main__":
