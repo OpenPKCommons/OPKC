8fc943b2002707210fd6907b6be26a6559223a10
fultoncarolyn <carolyn.fulton@colorado.edu>
2025-10-07 09:41:52 -0600

    update to wagstaffe2024

--- code/ingest_studies/studies/wagstaffe2024.py
+++ code/ingest_studies/studies/wagstaffe2024.py
@@ -3,59 +3,57 @@ from schema import enforce_schema, coerce_types
 
 def load_and_format():
     # Import the raw data:
-    df = pd.read_csv("data/ke2022.csv")
+    df = pd.read_csv("data/wagstaffe2024.csv")
 
     # Keep only the columns we need: 
-    df = df[['Ind', 'Time', 'Lineage', 'Nasal_CN', 'Saliva_Ct', 'Antigen', 'Age']]
-
-    # Clean up the Ind column: 
-    df["Ind"] = df["Ind"].str.replace(r"\s*\*", "", regex=True)
-
-    # Pivot the test outcome columns into a single column: 
-    df = df.melt(
-        id_vars=[col for col in df.columns if col not in ["Nasal_CN", "Saliva_Ct", "Antigen"]],
-        value_vars=["Nasal_CN", "Saliva_Ct", "Antigen"],
-        var_name="SampleType",
-        value_name="Log10VL"
-        )
+    df = df[['PersonID', 'DaysPostInoculation', 'GEml', 'site']]
+    # for each individual we have 1 to 19.5 DaysPostInoculation data points with corresponding GEml (NA if not available)
+    # Need to address:
+    # 1. are individual IDs same for nose and throat samples or independent?
+    # 2. how is GEml measured? What conversions need to be made?
+    # 3. how does GEml col get converted?
 
     # Map the contents of column SampleType to standard names: 
-    df["SampleType"] = df["SampleType"].replace({
-        "Nasal_CN": "nasal",
-        "Saliva_Ct": "saliva",
-        "Antigen": "antigen"
+    df["site"] = df["site"].replace({
+        "nose": "nasal",
+        "throat": "saliva" # need to confirm vs oropharyngeal
         })
 
     # Rename columns to match schema: 
     df = df.rename(columns={
-        "Ind": "PersonID",
-        "Time": "TimeDays",
-        "Lineage": "Subtype",
-        "Age": "AgeRng1"
+        "PersonID": "PersonID",
+        "DaysPostInoculation": "TimeDays",
+        "site": "SampleType"
         })
 
     # Add additional columns with known but missing information:
-    df["StudyID"] = "ke2022"
-    df["AgeRng2"] = df["AgeRng1"]
-    df["DOI"] = "10.1038/s41564-022-01105-z"
-    df["Units"] = df["SampleType"].map({
-        "saliva": "Ct",
-        "nasal": "Ct",
-        "antigen": "binary"
-        })
-    df["Platform"] = df["SampleType"].map({
-        "saliva": "Taqpath",
-        "nasal": "Alinity",
-        "antigen": "Sofia"
-        })
-    df["GEml_conversion_intercept"] = df["SampleType"].map({
-        "saliva": 14.24,
-        "nasal": 11.35,
-        })
-    df["GEml_conversion_slope"] = df["SampleType"].map({
-        "saliva": -0.28,
-        "nasal": -0.25,
-        })
+    df["StudyID"] = "wagstaffe2024"
+    df["DOI"] = "10.1016/S1473-3099(24)00183-X"
+    df["Units"] = "GEml"
+    # ACTIVATION TIME
+        # throat: 1.78 days
+        # nose: 2.61 days
+    # VIRAL LOAD GROWTH RATE
+        # throat: 5.41 days^-1
+        # nose: 4.86 days^-1
+    # PEAK TIME (ESTIMATED)
+        # throat: 3.4 days
+        # nose: 5.1 days
+    # PEAK VIRAL LOAD
+        # throat: 6.96 log_10
+        # nose: 8.69 log_10
+    # VIRAL LOAD DECAY RATE
+        # throat: 0.69 days^-1
+        # nose: 1.29 days^-1
+
+    # df["GEml_conversion_intercept"] = df["SampleType"].map({
+    #     "saliva": 0.0, # need to find!
+    #     "nasal": 0.0, # need to find!
+    #     })
+    # df["GEml_conversion_slope"] = df["SampleType"].map({
+    #     "saliva": 0.0, # need to find!
+    #     "nasal": 0.0, # need to find!
+    #     })
 
     df = enforce_schema(df)
     df = coerce_types(df)
